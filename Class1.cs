e8ebe58cdbb7c3d4a10ca8108c226991cba7151d
SalamMaleku <79752573+SalamMaleku@users.noreply.github.com>
2021-03-12 20:06:57 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static double Sum(double a, double b)
+        {
+            double c = a + b;
+            return c;
+        }
+        public static double Difference(double a, double b)
+        {
+            double c = a - b;
+            return c;
+        }
+        public static double Degree(double a, double b)
+        {
+            double c = a / b;
+            return c;
+        }
+
+        public static double Square(double a)
+        {
+            a *= a;
+            return a;
+        }
+        public static double Multiplication(double a, double b)
+        {
+            double c = a * b;
+            return c;
+        }
+        public static double Stepen(double a, double b)
+        {
+            a = Pow(a, b);
+            return a;
+        }
+        
+
+        public static double Sqrt1(double a)
+        {
+            a = Sqrt(a);
+            return a;
+        }
+        public static double Rounding(double a)
+        {
+            a = Round(a);
+            return a;
+        }
+        public static double AreaCircle(double r)
+        {
+            double S = PI * Pow(r, 2);
+            return S;
+        }
+        public static double SquareArea(double a)
+        {
+            a = Pow(a, 2);
+            return a;
+        }
+        //10
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using static System.Math;
+
+namespace aye2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double a = 1;
+            double b = 3;
+            var c = Class1.Sum(a, b);
+            Assert.AreEqual(4, c);
+        } //Сумма
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double a = 5;
+            double b = 1;
+            var c = Class1.Difference(a, b);
+            Assert.AreEqual(4, c);
+        } //Разность
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double a = 10;
+            double b = 5;
+            var c = Class1.Degree(a, b);
+            Assert.AreEqual(2, c);
+        }//Деление
+        [TestMethod]
+        public void TestMethod4()
+        {
+            double a = 10;
+            var c = Class1.Square(a);
+            Assert.AreEqual(100, c);
+        }//Площадь
+        [TestMethod]
+        public void TestMethod5()
+        {
+            double a = 10;
+            double b = 5;
+            var c = Class1.Multiplication(a, b);
+            Assert.AreEqual(50, c);
+        }//Умножение
+        [TestMethod]
+        public void TestMethod6()
+        {
+            double a = 5;
+            double b = 2;
+            var c = Class1.Stepen(a, b);
+            Assert.AreEqual(25, c);
+        }//Возведение в степень
+        [TestMethod]
+        public void TestMethod7()
+        {
+            double a = 9;
+            var c = Class1.Sqrt1(a);
+            Assert.AreEqual(3, c);
+        }// Квадратный корень
+        [TestMethod]
+        public void TestMethod8()
+        {
+            double a = 5.3;
+            var c = Class1.Rounding(a);
+            Assert.AreEqual(5, c);
+        }//Округление
+        [TestMethod]
+        public void TestMethod9()
+        {
+            double r = 2;
+            var c = Class1.AreaCircle(r);
+            Assert.AreEqual(4 * PI, c);
+        }//Площадь кр
+        [TestMethod]
+        public void TestMethod10()
+        {
+            double a = 10;
+            var c = Class1.SquareArea(a);
+            Assert.AreEqual(100, c);
+        }//Площадь квадрата
+    }
+}
